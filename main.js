804c3efc33f05d38e97b89be41fe90f13fcdc860
unknown <bobby.jovanovski@abv.bg>
2022-06-03 00:54:27 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,192 @@
+function functionA1() {
+    document.getElementById("whiteRookA1").style.position = "absolute";
+    document.getElementById("whiteRookA1").style.top = "370px";
+    document.getElementById("whiteRookA1").style.left = "70px";
+}
+
+function functionB1() {
+    document.getElementById("whiteKnightB1").style.position = "absolute";
+    document.getElementById("whiteKnightB1").style.top = "450px";
+    document.getElementById("whiteKnightB1").style.left = "230px";
+}
+
+function functionC1() {
+    document.getElementById("whiteBishopC1").style.position = "absolute";
+    document.getElementById("whiteBishopC1").style.top = "450px";
+    document.getElementById("whiteBishopC1").style.left = "70px";
+}
+
+function functionD1() {
+    document.getElementById("whiteQueenD1").style.position = "absolute";
+    document.getElementById("whiteQueenD1").style.top = "370px";
+    document.getElementById("whiteQueenD1").style.left = "310px";
+}
+
+function functionE1() {
+    document.getElementById("whiteKingE1").style.position = "absolute";
+    document.getElementById("whiteKingE1").style.top = "370px";
+    document.getElementById("whiteKingE1").style.left = "390px";
+}
+
+function functionF1() {
+    document.getElementById("whiteBishopF1").style.position = "absolute";
+    document.getElementById("whiteBishopF1").style.top = "370px";
+    document.getElementById("whiteBishopF1").style.left = "230px";
+}
+
+function functionG1() {
+    document.getElementById("whiteKnightG1").style.position = "absolute";
+    document.getElementById("whiteKnightG1").style.top = "450px";
+    document.getElementById("whiteKnightG1").style.left = "470px";
+}
+
+function functionH1() {
+    document.getElementById("whiteRookH1").style.position = "absolute";
+    document.getElementById("whiteRookH1").style.top = "370px";
+    document.getElementById("whiteRookH1").style.left = "630px";
+}
+
+function functionA2() {
+    document.getElementById("pawnA2").style.position = "absolute";
+    document.getElementById("pawnA2").style.top = "450px";
+    document.getElementById("pawnA2").style.left = "70px";
+}
+
+function functionB2() {
+    document.getElementById("pawnB2").style.position = "absolute";
+    document.getElementById("pawnB2").style.top = "450px";
+    document.getElementById("pawnB2").style.left = "150px";
+}
+
+function functionC2() {
+    document.getElementById("pawnC2").style.position = "absolute";
+    document.getElementById("pawnC2").style.top = "450px";
+    document.getElementById("pawnC2").style.left = "230px";
+}
+
+function functionD2() {
+    document.getElementById("pawnD2").style.position = "absolute";
+    document.getElementById("pawnD2").style.top = "450px";
+    document.getElementById("pawnD2").style.left = "310px";
+}
+
+function functionE2() {
+    document.getElementById("pawnE2").style.position = "absolute";
+    document.getElementById("pawnE2").style.top = "450px";
+    document.getElementById("pawnE2").style.left = "390px";
+}
+
+function functionF2() {
+    document.getElementById("pawnF2").style.position = "absolute";
+    document.getElementById("pawnF2").style.top = "450px";
+    document.getElementById("pawnF2").style.left = "470px";
+}
+
+function functionG2() {
+    document.getElementById("pawnG2").style.position = "absolute";
+    document.getElementById("pawnG2").style.top = "450px";
+    document.getElementById("pawnG2").style.left = "550px";
+}
+
+function functionH2() {
+    document.getElementById("pawnH2").style.position = "absolute";
+    document.getElementById("pawnH2").style.top = "450px";
+    document.getElementById("pawnH2").style.left = "630px";
+}
+
+
+function functionA7() {
+    document.getElementById("pawnA7").style.position = "absolute";
+    document.getElementById("pawnA7").style.top = "210px";
+    document.getElementById("pawnA7").style.left = "70px";
+}
+
+function functionB7() {
+    document.getElementById("pawnB7").style.position = "absolute";
+    document.getElementById("pawnB7").style.top = "210px";
+    document.getElementById("pawnB7").style.left = "150px";
+}
+
+function functionC7() {
+    document.getElementById("pawnC7").style.position = "absolute";
+    document.getElementById("pawnC7").style.top = "210px";
+    document.getElementById("pawnC7").style.left = "230px";
+}
+
+function functionD7() {
+    document.getElementById("pawnD7").style.position = "absolute";
+    document.getElementById("pawnD7").style.top = "210px";
+    document.getElementById("pawnD7").style.left = "310px";
+}
+
+function functionE7() {
+    document.getElementById("pawnE7").style.position = "absolute";
+    document.getElementById("pawnE7").style.top = "210px";
+    document.getElementById("pawnE7").style.left = "390px";
+}
+
+function functionF7() {
+    document.getElementById("pawnF7").style.position = "absolute";
+    document.getElementById("pawnF7").style.top = "210px";
+    document.getElementById("pawnF7").style.left = "470px";
+}
+
+function functionG7() {
+    document.getElementById("pawnG7").style.position = "absolute";
+    document.getElementById("pawnG7").style.top = "210px";
+    document.getElementById("pawnG7").style.left = "550px";
+}
+
+function functionH7() {
+    document.getElementById("pawnH7").style.position = "absolute";
+    document.getElementById("pawnH7").style.top = "210px";
+    document.getElementById("pawnH7").style.left = "630px";
+}
+
+function functionA8() {
+    document.getElementById("whiteRookA8").style.position = "absolute";
+    document.getElementById("whiteRookA8").style.top = "290px";
+    document.getElementById("whiteRookA8").style.left = "70px";
+}
+
+function functionB8() {
+    document.getElementById("whiteKnightB8").style.position = "absolute";
+    document.getElementById("whiteKnightB8").style.top = "210px";
+    document.getElementById("whiteKnightB8").style.left = "230px";
+}
+
+function functionC8() {
+    document.getElementById("whiteBishopC8").style.position = "absolute";
+    document.getElementById("whiteBishopC8").style.top = "210px";
+    document.getElementById("whiteBishopC8").style.left = "390px";
+}
+
+function functionD8() {
+    document.getElementById("whiteQueenD8").style.position = "absolute";
+    document.getElementById("whiteQueenD8").style.top = "210px";
+    document.getElementById("whiteQueenD8").style.left = "310px";
+}
+
+function functionE8() {
+    document.getElementById("whiteKingE8").style.position = "absolute";
+    document.getElementById("whiteKingE8").style.top = "280px";
+    document.getElementById("whiteKingE8").style.left = "390px";
+}
+
+function functionF8() {
+    document.getElementById("whiteBishopF8").style.position = "absolute";
+    document.getElementById("whiteBishopF8").style.top = "210px";
+    document.getElementById("whiteBishopF8").style.left = "630px";
+}
+
+function functionG8() {
+    document.getElementById("whiteKnightG8").style.position = "absolute";
+    document.getElementById("whiteKnightG8").style.top = "210px";
+    document.getElementById("whiteKnightG8").style.left = "470px";
+}
+
+function functionH8() {
+    document.getElementById("whiteRookH8").style.position = "absolute";
+    document.getElementById("whiteRookH8").style.top = "290px";
+    document.getElementById("whiteRookH8").style.left = "630px";
+}
